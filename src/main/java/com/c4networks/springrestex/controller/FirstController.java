b58692c60b2bfe0e282d2e3162a48b641ff43213
upenderm <upender0209@gmail.com>
2016-09-23 09:13:24 +0530

    Updated with controllers and jsp files

--- src/main/java/com/c4networks/springrestex/controller/FirstController.java
+++ src/main/java/com/c4networks/springrestex/controller/FirstController.java
@@ -2,14 +2,17 @@ package com.c4networks.springrestex.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FirstController {
 	
 	@RequestMapping("/hello")
-	public String sayHelloToUser(){
+	public ModelAndView sayHelloToUser(){
+		String message = "Welcome to your 1st Maven Spring project !";
+		System.out.println("from controller");
 		
-		return "Welcome";
+		return new ModelAndView("Welcome", "message", message);
 	}
 
 }
--- src/main/webapp/views/Welcome.jsp
+++ src/main/webapp/views/Welcome.jsp
@@ -4,13 +4,10 @@
 <title>Being Java Guys | Hello World</title>
 </head>
 <body>
-	<form action="hello" method="post">
+
 	<center>
 		<h2>Being Java Guys | Hello World</h2>
-		<h4>
-			<input type="submit" value="Click me">
-		</h4>
+		<h4>${message}</h4>
 	</center>
-	</form>
 </body>
 </html>
